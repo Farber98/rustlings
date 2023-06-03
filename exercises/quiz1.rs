57baf75db952b95cea1551bd4c4a728d936d703e
Farber98 <juantuc98@gmail.com>
2023-06-03 17:38:58 -0300

    finished functions, ifs, primitive_types and quiz1.

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,13 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(qty: u8) -> u8 {
+    if qty <= 40 {
+        return qty * 2;
+    }
+    qty
+}
 
 // Don't modify this function!
 #[test]

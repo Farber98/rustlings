d9afe2cc60a03429c10c62fe03dbc6c2fa81342c
Farber98 <juantuc98@gmail.com>
2023-06-04 23:55:50 -0300

    move semantics, structs and vecs done.

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -2,8 +2,6 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -27,6 +25,26 @@ fn create_order_template() -> Order {
     }
 }
 
+fn create_order(
+    name: &str,
+    year: u32,
+    made_by_phone: bool,
+    made_by_mobile: bool,
+    made_by_email: bool,
+    item_number: u32,
+    count: u32,
+) -> Order {
+    Order {
+        name: String::from(name),
+        year,
+        made_by_phone,
+        made_by_mobile,
+        made_by_email,
+        item_number,
+        count,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,7 +53,15 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = create_order(
+            "Hacker in Rust",
+            order_template.year,
+            order_template.made_by_phone,
+            order_template.made_by_mobile,
+            order_template.made_by_email,
+            order_template.item_number,
+            1,
+        );
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
